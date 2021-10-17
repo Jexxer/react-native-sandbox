a6be8e35a610fc1e3531aa0127d73bd5fb5f95dd
jexxer <jexxe@live.com>
2021-10-17 10:40:25 -0400

    initial commit on Welcome Screen

--- App.js
+++ App.js
@@ -1,55 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  Pressable,
-  Button,
-  Image,
-  SafeAreaView,
-} from "react-native";
-import axios from "axios";
+import { StyleSheet, Text, Pressable, SafeAreaView } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  const [imageList, setImageList] = useState([]);
-  const [currentImage, setCurrentImage] = useState(0);
-  const [currentImageURL, setCurrentImageURL] = useState(
-    "https://picsum.photos/200/300"
-  );
-
-  async function getImages() {
-    let imagesURLs = [];
-    let images = await axios.get("https://picsum.photos/v2/list?limit=100");
-    images.data.forEach((image) => imagesURLs.push(image.download_url));
-    setImageList(imagesURLs);
-  }
-
-  function handleClick() {
-    let currentNum = currentImage;
-    setCurrentImageURL(imageList[currentNum + 1]);
-    setCurrentImage(currentNum + 1);
-  }
-
-  useEffect(() => {
-    getImages();
-  }, []);
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Image
-        key={currentImageURL}
-        source={{
-          width: 300,
-          height: 600,
-          uri: currentImageURL,
-        }}
-      />
-      <Pressable style={styles.button} onPress={handleClick}>
-        <Text style={styles.text}>Click for new Image</Text>
-      </Pressable>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
 
 const styles = StyleSheet.create({
@@ -68,12 +23,5 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     elevation: 3,
     backgroundColor: "lightblue",
-  },
-  text: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: "bold",
-    letterSpacing: 0.25,
-    color: "white",
   },
 });
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,149 @@
+import React, { useState } from "react";
+import {
+  Alert,
+  ImageBackground,
+  Modal,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+function WelcomeScreen(props) {
+  const [modalVisible, setModalVisible] = useState();
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={{
+        uri: "https://image.cnbcfm.com/api/v1/image/106435772-1583893683724gettyimages-1162855475.jpeg?v=1604611798",
+      }}
+    >
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TextInput style={styles.input} placeholder="Username" />
+            <TextInput style={styles.input} placeholder="Password" />
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Login</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Text style={styles.headline}>Flint Finance</Text>
+      <View style={styles.container}>
+        <Pressable
+          style={styles.loginButton}
+          onPress={() => setModalVisible(!modalVisible)}
+        >
+          <View>
+            <Text style={styles.text}>Login</Text>
+          </View>
+        </Pressable>
+        <View style={styles.signupButton}>
+          <Text style={styles.text}>Sign up</Text>
+        </View>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: "contain",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  container: {
+    flexDirection: "row",
+  },
+  headline: {
+    color: "#6DCC94",
+    fontSize: 30,
+    fontWeight: "700",
+    position: "absolute",
+    top: "12%",
+  },
+  input: {
+    height: 40,
+    width: 150,
+    margin: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+  },
+  loginButton: {
+    width: "50%",
+    height: 70,
+    backgroundColor: "#6DCC94",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  signupButton: {
+    width: "50%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 30,
+    color: "#fff",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    width: 100,
+    borderRadius: 10,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+});
+
+export default WelcomeScreen;
